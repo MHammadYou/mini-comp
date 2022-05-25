e6813a502548b8d1d086457523ffdd923fb6b5e8
MHammadYou <MHammadY2.0@gmail.com>
2022-05-25 22:45:11 +0500

    Added Basic Lexer Error type

---
+++ src/lexer.rs
@@ -0,0 +1,23 @@
+
+use std::io;
+
+#[derive(Error, Debug)]
+pub enum LexerError {
+    #[error("")]
+    FileIO(#[from] io::Error),
+
+    MissingExpectedSymbol {
+        expected: ??,
+        found: ??
+    }
+}
+
+pub type Token = TokenType;
+
+pub enum TokenType {
+    EOF,
+    Punctuation { raw: char, kind: PunctuationKind},
+    Operations(String),
+    Identifier(String),
+    Char(char)
+}
