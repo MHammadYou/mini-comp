0966915ab6c9a7f4f884637b644a3d4b8afa9823
= <MHammadY2.0@gmail.com>
2022-06-17 19:21:47 +0500

    Refactored code

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub mod lexer;
 use lexer::*;
 
 fn main() {
-    let mut lexer = Lexer::new("{{}([[[[]]]]())}");
+    let mut lexer = Lexer::new("");
 
     loop {
         match lexer.next_token() {
