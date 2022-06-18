31f1645aa90a8ee2b9caffffb65700e8b1910682
= <MHammadY2.0@gmail.com>
2022-06-18 23:58:51 +0500

    Refactored code

--- src/lexer.rs
+++ src/lexer.rs
@@ -110,26 +110,16 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn parse_numbers(&mut self, start: char) -> Result<TokenType, LexerError> {
+
+    }
+
     fn transform_to_type(&mut self, c: char) -> Result<TokenType, LexerError> {
         match c {
-            '(' => Ok(TokenType::Punctuation {
-                raw: c, kind: PunctuationKind::Open(self.push_symbol(&c))
-            }),
-            ')' => Ok(TokenType::Punctuation {
-                raw: c, kind: PunctuationKind::Close(self.pop_symbol(&c)?)
-            }),
-            '{' => Ok(TokenType::Punctuation {
-                raw: c, kind: PunctuationKind::Open(self.push_symbol(&c))
-            }),
-            '}' => Ok(TokenType::Punctuation {
-                raw: c, kind: PunctuationKind::Close(self.pop_symbol(&c)?)
-            }),
-            '[' => Ok(TokenType::Punctuation {
-                raw: c, kind: PunctuationKind::Open(self.push_symbol(&c))
-            }),
-            ']' => Ok(TokenType::Punctuation {
-                raw: c, kind: PunctuationKind::Close(self.pop_symbol(&c)?)
-            }),
+            '(' | '[' => Ok(TokenType::Punctuation { raw: c, kind: PunctuationKind::Open(self.push_symbol(&c)) }),
+            ')' | ']' => Ok(TokenType::Punctuation { raw: c, kind: PunctuationKind::Close(self.pop_symbol(&c)?) }),
+            '0' ..= '9' => self.parse_numbers(c),
+            
             _ => Err(LexerError::UnknownSymbol { symbol: c.to_string() })
         }
     }
