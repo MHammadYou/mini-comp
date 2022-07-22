d8ced4ac5d58b878da68b2238c002c41a255e215
= <MHammadY2.0@gmail.com>
2022-07-22 23:35:08 +0500

    Added clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
+extern crate clap;
+
 pub mod lexer;
 pub mod parser;
 
 use lexer::*;
 use lexer::lexer::Lexer;
 
+use std::fs::File;
+use clap::{App, SubCommand};
 
-fn main() {
-    let mut lexer = Lexer::new("(22 2.22 11)[[[]]()]\"Test String\"");
+fn main() -> std::io::Result<()> {
+
+    let matches = App::new("MiniComp")
+        .subcommand(SubCommand::with_name("debug").args_from_usage(
+            "
+            --show...       'show certain steps in compiling process'
+            <INPUT>         'File to load'
+            "
+        ))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("debug", Some(sub_matches)) => {
+            let filename = sub_matches.value_of("INPUT").unwrap();
+            let text = std::fs::read_to_string(filename)?;
+            let lexer = Lexer::new(&text);
+        },
+        _ => {}
+    }
+    // TODO: will get back here
+
+    let mut lexer = Lexer::new(&*data);
 
     loop {
         match lexer.next_token() {
