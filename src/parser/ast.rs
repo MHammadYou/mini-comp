9194b4847fe9db81449473ad9cc270fe83408236
MHammadYou <MHammadY2.0@gmail.com>
2022-08-03 22:40:26 +0500

    Refactored code

---
+++ src/parser/ast.rs
@@ -0,0 +1,11 @@
+pub struct Program {
+
+}
+
+pub enum Literal {
+    Integer(i32),
+    FloatingPoint(f64),
+    String(String),
+    Boolean(bool)
+}
+
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,13 +1,3 @@
 pub mod ast;
 
 
-pub struct Program {
-
-}
-
-pub enum Literal {
-    Integer(i32),
-    FloatingPoint(f64),
-    String(String),
-    Boolean(bool)
-}
