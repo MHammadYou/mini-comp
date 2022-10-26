4e87d88ecb778841e035b679ebb2d64ee3150237
MHammadYou <MHammadY2.0@gmail.com>
2022-10-26 21:03:52 +0500

    Added return stmt

--- src/parser/stmt.rs
+++ src/parser/stmt.rs
@@ -9,6 +9,7 @@ pub enum Stmt {
     Let{ token: TokenType, initilizer: Expr },
     If{ condition: Expr, branch: Box<Stmt> },
     While{ condition: Expr, body: Box<Stmt> },
-    Function{ name: TokenType, params: Vec<TokenType>, body: Vec<Stmt> }
+    Function{ name: TokenType, params: Vec<TokenType>, body: Vec<Stmt> },
+    Return{ keyword: TokenType, value: Expr }
 }
 
