3af7c44ba327bbd3622604756135dc52bcb73854
MHammadYou <MHammadY2.0@gmail.com>
2023-01-04 17:22:27 +0500

    Refactored code

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -631,8 +631,7 @@ impl Parser {
         return false;
     } 
 
-    // TODO
-    fn _match_look_ahead(&mut self, types: &[&TokenType]) -> bool {
+    fn match_look_ahead(&mut self, types: &[&TokenType]) -> bool {
         for token_type in types {
             if self.current + 1 >= self.tokens.len() {
                 return false
